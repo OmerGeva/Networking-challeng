c2dbec65cd2d4d6ebcb2104cead2a9809ddee139
Omer Geva <ogeva97@gmail.com>
2019-12-15 18:17:49 +0200

    validation fix

--- app/models/profile_skill.rb
+++ app/models/profile_skill.rb
@@ -2,7 +2,7 @@ class ProfileSkill < ApplicationRecord
   belongs_to :profile
   belongs_to :skill
 
-  validates :skill, uniqueness: true
+  validates :skill, presence: true
 
   validates :experience, presence: true, inclusion: { in: (0..5) },
                      numericality: { only_integer: true }
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -4,7 +4,6 @@ class Skill < ApplicationRecord
 
   has_many :profiles, through: :profile_skills
 
-  validates :title, uniqueness: true
 
 
   include PgSearch::Model
