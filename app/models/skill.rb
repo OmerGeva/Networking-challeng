896a7b6a9d8bd54c1e4b5e8813deb5d1d11b18a1
Omer Geva <ogeva97@gmail.com>
2019-12-15 16:49:00 +0200

    heroku fix

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -2,7 +2,7 @@ class Skill < ApplicationRecord
   has_many :profile_skills
 
 
-  has_many :profiles, through: :profile_skills, dependant: :destroy
+  has_many :profiles, through: :profile_skills
 
   validates :title, uniqueness: true
 
